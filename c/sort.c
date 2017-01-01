8413c423e2bbfa034d515d2a98735511fe901830
hansung <hansung080@hanmail.net>
2017-01-01 17:43:14 +0900

    added insertion sort

--- c/sort.c
+++ c/sort.c
@@ -4,17 +4,20 @@
 #define TRUE  1
 #define FALSE 0
 
-#define SORT_TYPE   unsigned char
-#define BUBBLE_SORT 0
+#define SORT_TYPE      unsigned char
+#define INVALID_TYPE   0
+#define BUBBLE_SORT    1
+#define INSERTION_SORT 2
 
 #define SWAP(x, y, t) ((t)=(x)), ((x)=(y)), ((y)=(t))
 
-int a2i(const char* str);
+int atoi_hs(const char* str);
 void init_arr(int* arr, const char** argv, int len);
 void sort_arr(int* arr, int len, SORT_TYPE type);
 void print_arr(const int* arr, int len);
 
 void proc_bubble_sort(int* arr, int len);
+void proc_insertion_sort(int* arr, int len);
 
 int main(int argc, const char** argv){
 	if(argc < 2){
@@ -27,14 +30,14 @@ int main(int argc, const char** argv){
 
 	init_arr(arr, argv, len);	
 
-	sort_arr(arr, len, BUBBLE_SORT);
+	sort_arr(arr, len, INSERTION_SORT);
 
 	print_arr(arr, len);
 
 	return 0;
 }
 
-int a2i(const char* str){
+int atoi_hs(const char* str){
 	int i;
 	int result = 0;
 
@@ -50,7 +53,7 @@ void init_arr(int* arr, const char** argv, int len){
 	int i;
 
 	for(i = 0; i < len; i++){
-		arr[i] = a2i(argv[i+1]);
+		arr[i] = atoi_hs(argv[i+1]);
 	}
 }
 
@@ -58,6 +61,10 @@ void sort_arr(int* arr, int len, SORT_TYPE type){
 	switch(type){
 	case BUBBLE_SORT:
 		proc_bubble_sort(arr, len);	
+		break;
+	
+	case INSERTION_SORT:
+		proc_insertion_sort(arr, len);
 		break;
 
 	default:
@@ -83,14 +90,34 @@ void proc_bubble_sort(int* arr, int len){
 	int i, j, temp;
 	BOOL swaped = TRUE;
 
-	for(i = (len - 1); i > 0 && swaped == TRUE; i--){
+	for(i = len - 1; i > 0 && swaped == TRUE; i--){
 		swaped = FALSE;
+
 		for(j = 0; j < i; j++){
 			if(arr[j] > arr[j+1]){
 				SWAP(arr[j], arr[j+1], temp);
 				swaped = TRUE;
 			}
 		}
+	}
+}
+
+void proc_insertion_sort(int* arr, int len){
+	int i, j, key;
+	
+	for(i = 1; i < len; i++){
+		key = arr[i];	
+
+		for(j = i - 1; j >= 0; j--){
+			if(arr[j] > key){
+				arr[j+1] = arr[j];
+
+			}else{
+				break;				
+			}
+		}
+
+		arr[j+1] = key;
 	}
 }
 
