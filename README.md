e903ce4a35b8b52add59c045bde51fb322b0a317
hansung <hansung080@hanmail.net>
2017-01-02 20:53:24 +0900

    added square, square root

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ Algorithms and Data Structures
   - c
 
 @ algorithms :: c
+  - util : swap, strlen, atoi, atof
+  - math : round, floor, ceil, square, square root, square root(r) 
   - sort : bubble sort, insertion sort, quick sort
   - reverse
-  - rank : linear search, binary search, binary search recursive
+  - rank : linear search, binary search, binary search(r)
   - pyramid
 
 @ data structures :: c
--- c/math.h
+++ c/math.h
@@ -4,6 +4,10 @@
 double round(double x);
 double floor(double x);
 double ceil(double x);
+double square(double x);
+double square_root(double x);
+double square_root_r(double x);
+static double square_root_rec(double min, double max, double x);
 
 double round(double x){
 	return (int)(x + 0.5);
@@ -17,4 +21,53 @@ double ceil(double x){
 	return (int)(x + 0.9);
 }
 
+double square(double x){
+	return x * x;
+}
+
+double square_root(double x){
+	double min = 0.0;
+	double max = x;
+	double mid;
+
+	while(min <= max){
+		mid = (min + max) / 2.0;	
+		
+		if(mid * mid == x){
+			return mid;
+
+		}else if(mid * mid > x){
+			max = mid;
+
+		}else{
+			min = mid;	
+		}
+	}
+
+	return -1.0;
+}
+
+double square_root_r(double x){
+	return square_root_rec(0, x, x);	
+}
+
+static double square_root_rec(double min, double max, double x){
+	if(min > max){
+		return -1.0;
+	}
+	
+	double mid = (min + max) / 2.0;
+
+	if(mid * mid == x){
+		return mid;
+	
+	}else if(mid * mid > x){
+		return square_root_rec(min, mid, x);
+
+	}else{
+		return square_root_rec(mid, max, x);
+	}
+}
+
 #endif // __MATH_H__
+
