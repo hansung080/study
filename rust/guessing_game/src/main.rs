c3f2444968b2ba59d66c5d4a52dc74782d7d3479
hansung <hansung080@hanmail.net>
2020-05-12 19:46:31 +0900

    added guess range checking logic

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -1,6 +1,47 @@
+mod guess {
+    use std::cmp::Ordering;
+
+    pub struct Guess {
+        value: i32,
+    }
+
+    impl Guess {
+        pub fn new(value: i32) -> Result<Guess, Error> {
+            if value < 1 || value > 100 {
+                return Err(Error::new(String::from("Number must be between 1 and 100")))
+            }
+
+            Ok(Guess { value })
+        }
+
+        pub fn value(&self) -> i32 {
+            self.value
+        }
+
+        pub fn cmp(&self, other: &i32) -> Ordering {
+            self.value.cmp(other)
+        }
+    }
+
+    pub struct Error {
+        msg: String,
+    }
+
+    impl Error {
+        pub fn new(msg: String) -> Error {
+            Error { msg }
+        }
+
+        pub fn msg(&self) -> &String {
+            &self.msg
+        }
+    }
+}
+
 use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
+use crate::guess::Guess;
 
 fn main() {
     println!("###################################");
@@ -22,11 +63,19 @@ fn main() {
             },
         };
     
+        let guess = match Guess::new(guess) {
+            Ok(guess) => guess,
+            Err(error) => {
+                println!("{}.", error.msg());
+                continue;
+            }
+        };
+
         match guess.cmp(&answer) {
-            Ordering::Less => println!("{} is LESS than the answer.", guess),
-            Ordering::Greater => println!("{} is GREATER than the answer.", guess),
+            Ordering::Less => println!("{} is LESS than the answer.", guess.value()),
+            Ordering::Greater => println!("{} is GREATER than the answer.", guess.value()),
             Ordering::Equal => {
-                println!("{} is CORRECT. Congratulations~!!", guess);
+                println!("{} is CORRECT. Congratulations~!!", guess.value());
                 break;
             }
         }
