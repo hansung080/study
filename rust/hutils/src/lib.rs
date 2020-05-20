cb446a56c6d5b375315c93994c0d2b92678b7106
hansung <hansung080@hanmail.net>
2020-05-20 19:22:23 +0900

    added description and license to Cargo.toml

--- rust/hutils/src/lib.rs
+++ rust/hutils/src/lib.rs
@@ -1,7 +1,7 @@
 // Copyright (c) The Hutils Contributors
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-//! `hutils` provies a variety of utilities to help you write a code easier and better.
+//! `hutils` provies various utilities to help you write a code easier and better.
 
 pub mod cache;
 
