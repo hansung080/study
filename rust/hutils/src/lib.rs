8a14d33c39175b619e7a338f90b4b419672d19cc
hansung <hansung080@hanmail.net>
2020-05-22 17:29:04 +0900

    deleted all unnecessary files

--- rust/hutils/src/lib.rs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) The Hutils Contributors
-// SPDX-License-Identifier: MIT OR Apache-2.0
-
-//! `hutils` provies various utilities to help you write a code easier and better.
-
-pub mod cache;
-
-pub use self::cache::ClosureCacher;
