4ef12e0ff55497a3ffa9e08fc976882abe7665cf
hansung <hansung080@hanmail.net>
2020-06-14 18:22:17 +0900

    added state-pattern

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Study on languages, data structures, and algorithms
 # Rust
 - Guessing Game
 - Minigrep
+- State Pattern
 
 # JavaScript
 - Basic - Hello, Hello5, Pyramid, Constructor, Inheritance, Revolution, Moving Letters
---
+++ rust/state-pattern/src/polymorphism.rs
@@ -0,0 +1,18 @@
+mod blog;
+
+use blog::Post;
+
+pub fn run() {
+    let mut post = Post::new();
+    
+    post.add_text("I ate a salad for lunch today.");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today.", post.content());
+
+    println!("State pattern using polymorphism completed.");
+}
